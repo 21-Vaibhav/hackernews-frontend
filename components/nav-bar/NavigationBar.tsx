a1a2a86690c20213d1e918d68047a21f716a28df
Vaibhav G <vaibhavvishnu456@gmail.com>
2025-04-29 10:10:28 +0530

    Update NavigationBar component: change background color to #FF6600 for consistency

--- components/nav-bar/NavigationBar.tsx
+++ components/nav-bar/NavigationBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NavigationBar: React.FC = () => {
   return (
-    <div className="max-w-7xl mx-auto bg-orange-600 text-black flex items-center justify-between px-4 py-1 text-sm font-sans mt-5 ">
+    <div className="max-w-7xl mx-auto bg-[#FF6600] text-black flex items-center justify-between px-4 py-1 text-sm font-sans mt-5 ">
       <div className="flex items-center space-x-2">
         <div className="bg-white text-orange-600 font-bold px-1">Y</div>
         <span className="font-bold">Hacker News</span>
