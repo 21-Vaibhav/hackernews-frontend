f6e6c7a7bead7e8a051ea3c499661e6c905a19ed
Vaibhav G <vaibhavvishnu456@gmail.com>
2025-04-29 09:00:36 +0530

    Update NavigationBar component: change text color from white to black for better visibility

--- components/nav-bar/NavigationBar.tsx
+++ components/nav-bar/NavigationBar.tsx
@@ -3,35 +3,35 @@ import React from "react";
 
 const NavigationBar: React.FC = () => {
   return (
-    <div className="max-w-7xl mx-auto bg-orange-600 text-white flex items-center justify-between px-4 py-1 text-sm font-sans mt-5 ">
+    <div className="max-w-7xl mx-auto bg-orange-600 text-black flex items-center justify-between px-4 py-1 text-sm font-sans mt-5 ">
       <div className="flex items-center space-x-2">
         <div className="bg-white text-orange-600 font-bold px-1">Y</div>
         <span className="font-bold">Hacker News</span>
-        <a href="#" className="text-white hover:underline">
+        <a href="#" className="text-black hover:underline">
           new
         </a>
         <span>|</span>
-        <a href="#" className="text-white hover:underline">
+        <a href="#" className="text-black hover:underline">
           past
         </a>
         <span>|</span>
-        <a href="#" className="text-white hover:underline">
+        <a href="#" className="text-black hover:underline">
           comments
         </a>
         <span>|</span>
-        <a href="#" className="text-white hover:underline">
+        <a href="#" className="text-black hover:underline">
           ask
         </a>
         <span>|</span>
-        <a href="#" className="text-white hover:underline">
+        <a href="#" className="text-black hover:underline">
           show
         </a>
         <span>|</span>
-        <a href="#" className="text-white hover:underline">
+        <a href="#" className="text-black hover:underline">
           jobs
         </a>
         <span>|</span>
-        <a href="#" className="text-white hover:underline">
+        <a href="#" className="text-black hover:underline">
           submit
         </a>
       </div>
